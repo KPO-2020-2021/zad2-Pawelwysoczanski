d78e43b9eb9c4ae713c41960352175511c670bc2
Pawelwysoczanski <80000484+Pawelwysoczanski@users.noreply.github.com>
2021-03-31 20:41:19 +0200

    Update Statystyki.hh

--- inc/Statystyki.hh
+++ inc/Statystyki.hh
@@ -1,13 +1,26 @@
-#ifndef WYRAZENIEZESP_HH
-#define WYRAZENIEZESP_HH
-
-
+#ifndef STATYSTYKI_HH
+#define STATYSTYKI_HH
 
 #include "LZespolona.hh"
 #include "WyrazenieZesp.hh"
 
 /*
- * Tu nalezy zdefiniowac funkcje, ktore definiuja model statystyki 
+ * Modeluje pojecie Statystyka zawierajacej ilosc poprawnych
+ * i blednych odpowiedzi jednoczesnie wyciagajac wynik procentowy 
+ * testu
  */
+struct Statystyka
+{
+    int poprawna;    /*Ilosc poprawnych odpowiedzi*/
+    int niepoprawna; /*Ilosc niepoprawnych odpowiedzi*/
+    double procent;
+};
+
+void DodajPoprawna(Statystyka &odp);    /*Funkcja zliczajaca ilosc poprawnych odpowiedzi*/
+void DodajNiepoprawna(Statystyka &odp); /*Funkcja zliczajaca ilosc blednych odpowiedzi*/
+void Wykonaj();                         /*Funkcja wykonujaca podsumowanie poprze wyliczenie wyniku procentowego odpowiedzi poprawnych do sumy mozliwych */
+void WyswietlWyniki(Statystyka odp);   /*Funkcja wyswietlajaca podsumowanie*/
+void Inicjalizuj(Statystyka odp);
+std::ostream &operator<<(std::ostream StrmWyj, Statystyka odp);
 
 #endif

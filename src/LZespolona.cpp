ae51cb06ef43fff13a3ab7d8223f1d1faecacae7
Pawelwysoczanski <80000484+Pawelwysoczanski@users.noreply.github.com>
2021-03-31 20:44:10 +0200

    Update LZespolona.cpp

--- src/LZespolona.cpp
+++ src/LZespolona.cpp
@@ -1,10 +1,6 @@
 #include "LZespolona.hh"
-#include <iostream>
-#include <cmath>
 
-using namespace std;
-
-#define MIN_DIFF 0.00001
+#define MIN_DIFF 0.01
 
 /*!
  * Realizuje porównanie dwoch liczb zespolonych.
@@ -17,17 +13,21 @@ using namespace std;
 
 bool operator==(LZespolona Skl1, LZespolona Skl2)
 {
-  if ((Skl1.re == Skl2.re) && (Skl1.im == Skl2.im))
+  /*  if ((Skl1.re == Skl2.re) && (Skl1.im == Skl2.im))
     return true;
   else
     return false;
+*/
   //alternatywnie, dla MIN_DIFF i wyników od użytkownika
-  /*
-  if abs(Skl1.re - Skl2.re) <= MIN_DIFF && abs(Skl1.im - Skl2.im) <= MIN_DIFF
+
+  if ((std::abs(Skl1.re - Skl2.re) <= MIN_DIFF) && (std::abs(Skl1.im - Skl2.im) <= MIN_DIFF))
+  {
     return true;
+  }
   else
+  {
     return false;
-  */
+  }
 }
 
 /*!
@@ -98,22 +98,6 @@ LZespolona operator-(LZespolona Skl1, LZespolona Skl2)
 }
 
 /*!
- * Realizuje dzielenie liczby zespolonej przez skalar.
- * Argumenty:
- *    Skl1 - dzielona liczba zespolona,
- *    Skl2 - skalar-dzielnik.
- * Zwraca:
- *    Wynik dzielenia dwoch skladnikow przekazanych jako parametry.
- */
-LZespolona operator/(LZespolona Skl1, double Skl2)
-{
-  LZespolona Wynik;
-
-  Wynik.re = Skl1.re / Skl2;
-  Wynik.im = Skl1.im / Skl2;
-  return Wynik;
-}
-/*!
  * Realizuje dzielenie dwoch liczb zespolonych.
  * Argumenty:
  *    Skl1 - pierwszy skladnik dzielenia,
@@ -157,6 +141,53 @@ LZespolona operator*(LZespolona Skl1, LZespolona Skl2)
 void Wyswietl(LZespolona Skl1)
 {
 
-  cout << "(" << Skl1.re << showpos << Skl1.im << "i"
-       << ")" << endl;
+  std::cout << "(" << Skl1.re << std::showpos << Skl1.im << "i"
+            << ")" << std::endl;
+}
+
+/*!
+ * Realizuje dzielenie liczby zespolonej przez skakar.
+ * Argumenty:
+ *    Skl1 - dzielona liczba zespolona,
+ *    Skl2 - skalar-dzielnik.
+ * Zwraca:
+ *    Wynik dzielenia dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona operator/(LZespolona Skl1, double Skl2)
+{
+  if (Skl2 == 0)
+  {
+    throw std::runtime_error("Math error: Attempted to divide by Zero\n");
+  }
+  LZespolona Wynik;
+  Wynik.re = Skl1.re / Skl2;
+  Wynik.im = Skl1.im / Skl2;
+  return Wynik;
+}
+
+std::ostream &operator<<(std::ostream &StrmWyj, LZespolona LZesp)
+{
+  StrmWyj.precision(2);
+  StrmWyj << "(" << std::fixed << LZesp.re << std::showpos << LZesp.im << std::noshowpos << "i)";
+  return StrmWyj;
+}
+
+void WczytajTenZnak(std::istream &StrmWej, char Znak)
+{
+  char CzytanyZnak = ' ';
+  StrmWej >> CzytanyZnak;
+  if (CzytanyZnak != Znak)
+  {
+    StrmWej.setstate(std::ios::failbit);
+  }
+}
+std::istream &operator>>(std::istream &StrmWej, LZespolona &LZesp)
+{
+  //Korzystamy tutaj z tego, że jak strumień jest w stanie fail, to żadna operacja czytania już się nie wykona.
+  WczytajTenZnak(StrmWej, '(');
+  StrmWej >> LZesp.re;
+  StrmWej >> LZesp.im;
+  WczytajTenZnak(StrmWej, 'i');
+  WczytajTenZnak(StrmWej, ')');
+  return StrmWej;
 }

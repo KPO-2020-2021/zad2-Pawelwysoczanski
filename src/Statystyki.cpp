b4d519dec4b9baa868fcb26c7f62d9e491b06ea6
Pawelwysoczanski <80000484+Pawelwysoczanski@users.noreply.github.com>
2021-03-31 20:45:46 +0200

    Update Statystyki.cpp

--- src/Statystyki.cpp
+++ src/Statystyki.cpp
@@ -1,45 +1,119 @@
-#include "Statystyki.hh"
+#include <iostream>
+#include <cstring>
+#include <cassert>
 #include "BazaTestu.hh"
 
 /*
-*Funkcja odpowiadajaca za zliczanie dobrych odpowiedzi uzytkownika
-*/
-void DodajPoprawna(Statystyka &odp)
-{
-    odp.poprawna++;
-}
+ * Tablica, ktora jest widoczna tylko w tym module.
+ * Zawiera ona tresc latwego testu.
+ */
+static WyrazenieZesp TestLatwy[] =
+    {
+        {{2, 1}, Op_Dodaj, {1, 2}},
+        {{1, 0}, Op_Odejmij, {0, 1}},
+        {{3, 0}, Op_Mnoz, {0, 3}},
+        {{4, 8}, Op_Dziel, {1, 0}},
+};
+
 /*
-*Funkcja odpowiadajac za zliczanie blednych odpowiedzi uzytkownika
-*/
-void DodajNiepoprawna(Statystyka &odp)
-{
-    odp.niepoprawna++;
-}
+ * Analogicznie zdefiniuj test "trudne"
+ *
+ */
+static WyrazenieZesp TestTrudny[] =
+    {
+        {{6, 1}, Op_Dodaj, {1, 2}},
+        {{1, 0}, Op_Odejmij, {0, 1}},
+        {{3, 0}, Op_Mnoz, {0, 3}},
+        {{4, 8}, Op_Dziel, {1, 0}},
+};
+
 /*
-*Funkcja odpowiadajaca za wyliczenie wyniku procentowego
-*/
-double Wykonaj(Statystyka &odp)
+ * W bazie testu ustawia wybrany test jako biezacy test i indeks pytania
+ * ustawia na pierwsze z nich.
+ * Parametry:
+ *    wskBazaTestu - wskaznik na zmienna reprezentujaca baze testu,
+ *    wskTabTestu  - wskaznik na tablice zawierajaca wyrazenia arytmetyczne
+ *                   bedace przedmiotem testu,
+ *    IloscElemTab - ilosc pytan w tablicy.
+ *   
+ * Warunki wstepne:
+ *      - Parametr wskBazaTestu nie moze byc pustym wskaznikiem. Musi zawierac adres
+ *        zmiennej reprezentujacej baze testu, ktora wczesniej zostal poprawnie
+ *        zainicjalizowany poprzez wywolanie funkcji InicjalizujTest.
+ *      - Parametr wskTabTestu zawiera wskaznik na istniejaca tablice.
+ *      - Parametr IloscPytan zawiera wartosc, ktora nie przekracza ilosci elementow
+ *        w tablicy dostepnej poprzez wskTabTestu.
+ */
+void UstawTest(BazaTestu *wskBazaTestu, WyrazenieZesp *wskTabTestu, unsigned int IloscPytan)
 {
-    Statystyka Wynik;
-    Wynik.procent = odp.poprawna / (odp.poprawna + odp.niepoprawna);
-
-    return Wynik.procent;
+  wskBazaTestu->wskTabTestu = wskTabTestu;
+  wskBazaTestu->IloscPytan = IloscPytan;
+  wskBazaTestu->IndeksPytania = 0;
 }
+
 /*
-*Funkcja odpowiadajaca za wyswietlenie podsumowania sprawdzianu
-*/
-void WyswietlWyniki(Statystyka odp)
+ * Inicjalizuje test kojarzac zmienna dostepna poprzez wskaznik wskBazaTestu
+ * z dana tablica wyrazen, ktora reprezentuje jeden z dwoch dopuszczalnych 
+ * testow.
+ * Parametry:
+ *    wskBazaTestu - wskaznik na zmienna reprezentujaca baze testu.
+ *    sNazwaTestu  - wskaznik na napis wybranego typu testu.
+ *
+ * Warunki wstepne:
+ *      - Parametr wskBazaTestu nie moze byc pustym wskaznikiem. Musi zawierac adres
+ *        zmiennej reprezentujacej baze testu, ktora wczesniej zostal poprawnie
+ *        zainicjalizowany poprzez wywolanie funkcji InicjalizujTest.
+ *      - Parametr sNazwaTestu musi wskazywac na jedna z nazw tzn. "latwe" lub "trudne".
+ *       
+ * Zwraca:
+ *       true - gdy operacja sie powiedzie i test zostanie poprawnie
+ *              zainicjalizowany,
+ *       false - w przypadku przeciwnym.
+ */
+bool InicjalizujTest(BazaTestu *wskBazaTestu, const char *sNazwaTestu)
 {
-
-    double Wynik = Wykonaj(odp);
-    std::cout << "Ilosc dobrych odpowiedzi: " << odp.poprawna << std::endl;
-    std::cout << "Ilosc blednych odpowiedzi: " << odp.niepoprawna << std::endl;
-    std::cout << "Wynik w procentach: " << Wynik << "%" << std::endl;
-
+  if (!strcmp(sNazwaTestu, "latwy"))
+  {
+    UstawTest(wskBazaTestu, TestLatwy, sizeof(TestLatwy) / sizeof(WyrazenieZesp));
+    return true;
+  }
+  /*
+   * Analogicznie zrob inicjalizacje dla testu trudne
+   */
+  if (!strcmp(sNazwaTestu, "trudny"))
+  {
+    UstawTest(wskBazaTestu, TestTrudny, sizeof(TestTrudny) / sizeof(WyrazenieZesp));
+    return true;
+  }
+  std::cerr << "Otwarcie testu '" << sNazwaTestu << "' nie powiodlo sie." << std::endl;
+  return false;
 }
-void Inicjalizuj(Statystyka odp)
-{
 
-    odp.poprawna = 0;
-    odp.niepoprawna = 0;
+/*!
+ * Funkcja udostepnia nastepne pytania o ile jest odpowiednia ich ilosc.
+ * Parametry:
+ *       wskBazaTestu - wskaznik na zmienna reprezentujaca baze testu.
+ *       wskWyrazenie - wskaznik na zmienna, do ktorej zostanie wpisane
+ *                      kolejne wyrazenie z bazy testu.
+ *
+ * Warunki wstepne:
+ *      - Parametr wskBazaTestu nie moze byc pustym wskaznikiem. Musi zawierac adres
+ *        zmiennej reprezentujacej baze testu, ktora wczesniej zostal poprawnie
+ *        zainicjalizowany poprzez wywolanie funkcji InicjalizujTest.
+ *      - Parametr wskWyrazenie nie moze byc pustym wskaznikiem. Musi wskazywac na
+ *        istniejaca zmienna.
+ *
+ * Zwraca:
+ *       true - gdy operacja sie powiedzie i parametrowi *wskWyrazenie zostanie
+ *              przypisane nowe wyrazenie zespolone z bazy,
+ *       false - w przypadku przeciwnym.
+ */
+bool PobierzNastpnePytanie(BazaTestu *wskBazaTestu, WyrazenieZesp *wskWyrazenie)
+{
+  if (wskBazaTestu->IndeksPytania >= wskBazaTestu->IloscPytan)
+    return false;
+
+  *wskWyrazenie = wskBazaTestu->wskTabTestu[wskBazaTestu->IndeksPytania];
+  ++wskBazaTestu->IndeksPytania;
+  return true;
 }

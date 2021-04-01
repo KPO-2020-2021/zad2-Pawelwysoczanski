c2640a43b63d5677f42da8763fb5f70dd19b00cc
Pawelwysoczanski <80000484+Pawelwysoczanski@users.noreply.github.com>
2021-04-01 13:37:20 +0200

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -2,7 +2,8 @@
 #include "./doctest/doctest.h"
 #include "LZespolona.hh"
 
-TEST_CASE("test LZespolona Suma"){
+TEST_CASE("test LZespolona Suma 1")
+{
     LZespolona x, y, z;
 
     x.re = 1;
@@ -13,6 +14,61 @@ TEST_CASE("test LZespolona Suma"){
 
     z.re = 2;
     z.im = -2;
-    
-    CHECK(x+y == z);
+
+    CHECK(x + y == z);
+}
+TEST_CASE("test LZespolona Różnica")
+{
+    LZespolona x, y, z;
+
+    x.re = 3;
+    x.im = 4;
+
+    y.re = 2;
+    y.im = 1;
+
+    z.re = 1;
+    z.im = 3;
+
+    CHECK(x + y == z);
+}
+TEST_CASE("test LZespolona Iloczyn")
+{
+    LZespolona x, y, z;
+
+    x.re = 2;
+    x.im = 3;
+
+    y.re = 2;
+    y.im = 3;
+
+    z.re = -5;
+    z.im = 12;
+
+    CHECK(x * y == z);
+}
+TEST_CASE("test LZespolona Sprzezenie")
+{
+    LZespolona y, x;
+
+    x.re = 2;
+    x.im = 3;
+
+    y.re = 2;
+    y.im = -3;
+}
+TEST_CASE("test LZespolona Suma 2")
+{
+    LZespolona x, y, z;
+
+    x.re = 2;
+    x.im = 3;
+
+    y.re = 4;
+    y.im = 1;
+
+    z.re = 6;
+    z.im = 4;
+
+    CHECK((x += y) == z);
 }

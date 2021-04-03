cd87eab587187d95fac0cd0b89dd6447dbef8b82
Pawelwysoczanski <80000484+Pawelwysoczanski@users.noreply.github.com>
2021-04-03 15:57:31 +0200

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,65 +1,19 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "./doctest/doctest.h"
 #include "LZespolona.hh"
-
 TEST_CASE("test LZespolona Suma 1")
 {
     LZespolona x, y, z;
+    LZespolona a, b, c;
 
-    x.re = 1;
-    x.im = 1;
+    a.re = -1;
+    a.im = -2;
 
-    y.re = 1;
-    y.im = -3;
+    b.re = -3;
+    b.im = -4;
 
-    z.re = 2;
-    z.im = -2;
-
-    CHECK(x + y == z);
-}
-TEST_CASE("test LZespolona Różnica")
-{
-    LZespolona x, y, z;
-
-    x.re = 3;
-    x.im = 4;
-
-    y.re = 2;
-    y.im = 1;
-
-    z.re = 1;
-    z.im = 3;
-
-    CHECK(x + y == z);
-}
-TEST_CASE("test LZespolona Iloczyn")
-{
-    LZespolona x, y, z;
-
-    x.re = 2;
-    x.im = 3;
-
-    y.re = 2;
-    y.im = 3;
-
-    z.re = -5;
-    z.im = 12;
-
-    CHECK(x * y == z);
-}
-TEST_CASE("test LZespolona Sprzezenie")
-{
-    LZespolona y, x;
-
-    x.re = 2;
-    x.im = 3;
-
-    y.re = 2;
-    y.im = -3;
-}
-TEST_CASE("test LZespolona Suma 2")
-{
-    LZespolona x, y, z;
+    c.re = -4;
+    c.im = -6;
 
     x.re = 2;
     x.im = 3;
@@ -70,5 +24,198 @@ TEST_CASE("test LZespolona Suma 2")
     z.re = 6;
     z.im = 4;
 
+    CHECK(a + b == c);
+    CHECK(x + y == z);
+}
+TEST_CASE("test LZespolona Suma 2")
+{
+    LZespolona x, y, z;
+    LZespolona a, b, c;
+
+    a.re = -1;
+    a.im = -2;
+
+    b.re = 0.0;
+    b.im = 0.0;
+
+    c.re = -1;
+    c.im = -2;
+
+    x.re = 0.009;
+    x.im = 0.009;
+
+    y.re = 0.001;
+    y.im = 0.001;
+
+    z.re = 0.01;
+    z.im = 0.01;
+
+    CHECK(a + b == c);
+    CHECK(x + y == z);
+}
+TEST_CASE("test LZespolona Suma 3")
+{
+    LZespolona x, y, z;
+    LZespolona a, b, c;
+
+    a.re = -1;
+    a.im = -2;
+
+    b.re = -3;
+    b.im = -4;
+
+    c.re = -4;
+    c.im = -6;
+
+    x.re = 2;
+    x.im = 3;
+
+    y.re = 4;
+    y.im = 1;
+
+    z.re = 6;
+    z.im = 4;
+
+    CHECK((a += b) == c);
     CHECK((x += y) == z);
+}
+TEST_CASE("test LZespolona Suma 4")
+{
+    LZespolona x, y, z;
+    LZespolona a, b, c;
+
+    a.re = -1;
+    a.im = -2;
+
+    b.re = 0.0;
+    b.im = 0.0;
+
+    c.re = -1;
+    c.im = -2;
+
+    x.re = 0.009;
+    x.im = 0.009;
+
+    y.re = 0.001;
+    y.im = 0.001;
+
+    z.re = 0.01;
+    z.im = 0.01;
+
+    CHECK((a += b) == c);
+    CHECK((x += y) == z);
+}
+TEST_CASE("test LZespolona Różnica 1")
+{
+    LZespolona x, y, z;
+    LZespolona a, b, c;
+
+    a.re = -1;
+    a.im = -2;
+
+    b.re = -3;
+    b.im = -4;
+
+    c.re = 2;
+    c.im = 2;
+
+    x.re = 3;
+    x.im = 4;
+
+    y.re = 2;
+    y.im = 1;
+
+    z.re = 1;
+    z.im = 3;
+
+    CHECK(a - b == c);
+    CHECK(x - y == z);
+}
+TEST_CASE("test LZespolona Różnica 2")
+{
+    LZespolona x, y, z;
+    LZespolona a, b, c;
+
+    a.re = 0;
+    a.im = 0;
+
+    b.re = 0.0;
+    b.im = 0.0;
+
+    c.re = 0;
+    c.im = 0;
+
+    x.re = 0;
+    x.im = 0;
+
+    y.re = 0.00001;
+    y.im = 0.00001;
+
+    z.re = -0.00001;
+    z.im = -0.00001;
+
+    CHECK(a - b == c);
+    CHECK(x - y == z);
+}
+TEST_CASE("test LZespolona Iloczyn")
+{
+    LZespolona x, y, z;
+    LZespolona a, b, c;
+    LZespolona u, w, k;
+
+    a.re = -1;
+    a.im = -2;
+
+    b.re = 0;
+    b.im = 0;
+
+    c.re = 0;
+    c.im = 0;
+
+    x.re = 2;
+    x.im = 3;
+
+    y.re = 1;
+    y.im = 0;
+
+    z.re = 2;
+    z.im = 3;
+
+    u.re = 3;
+    u.im = 9;
+
+    w.re = 1;
+    w.im = 1;
+
+    k.re = -6;
+    k.im = 12;
+    CHECK(a * b == c);
+    CHECK(x * y == z);
+    CHECK(u * w == k);
+}
+TEST_CASE("test LZespolona Iloczyn")
+{
+    LZespolona x, y, z;
+    LZespolona a, b, c;
+
+    a.re = -1;
+    a.im = -2;
+
+    b.re = -3;
+    b.im = -4;
+
+    c.re = -5;
+    c.im = 10;
+
+    x.re = 2;
+    x.im = 3;
+
+    y.re = 2;
+    y.im = 3;
+
+    z.re = -5;
+    z.im = 12;
+
+    CHECK(a * b == c);
+    CHECK(x * y == z);
 }

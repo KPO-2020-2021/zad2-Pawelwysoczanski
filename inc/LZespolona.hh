8471fbacc662570134fbf18e539ee6130cf9ce0a
Pawelwysoczanski <80000484+Pawelwysoczanski@users.noreply.github.com>
2021-04-03 13:55:38 +0200

    Update LZespolona.hh

--- inc/LZespolona.hh
+++ inc/LZespolona.hh
@@ -19,45 +19,36 @@ struct LZespolona
   double re; /*! Pole repezentuje czesc rzeczywista. */
   double im; /*! Pole repezentuje czesc urojona. */
 
-  void Sprzezenie(); /*Fukcja tworzaca sprzezenie liczby zespolonej*/
+  LZespolona operator+(LZespolona Skl1); /*opearcja dodania dwoczh liczb zespolonych*/
+
+  LZespolona operator+=(LZespolona Skl1);
+
+  LZespolona operator-(LZespolona Skl1); /*operacja odjecia dwoch liczb zespolonych*/
+
+  LZespolona operator*(LZespolona Skl1); /* operacje mnozenia dwoch liczb zespolonych*/
+
+  LZespolona operator/(double Skl2); /* operacja dzielenia liczby zespolonej przez skalar*/
+
+  LZespolona operator/(LZespolona Skl1); /*operacja dzielenia dwoch liczb zespolonych*/
+
+  LZespolona operator/=(LZespolona Skl1); /*operacja dzielenia dwoch liczb zespolonych*/
+
+  LZespolona operator/=(double Skl2);/* operacja dzielenia liczby zespolonej przez skalar*/
+
+  double modul2(); /*Funkcja tworzaca modul w postaci skalarnej z liczby zespolonej podniesiony do kwadratu*/
+
+    LZespolona Sprzezenie(); /*Fukcja tworzaca sprzezenie liczby zespolonej*/
+
+  void Wyswietl();/*Funkcja wyswietlajaca liczbe zespolona*/
+
+  double arg();/*Funkcja odczytujaca wartosc Argumentu glownego liczby zespolonej*/
 };
-/*
-*  Przeciazenia operatorow arytmetycznych
-*/
 
-double arg(LZespolona Skl1);
+std::istream &operator>>(std::istream &StrmWej, LZespolona &Lzesp); /*Funkcja przeciazenia operatora przesuniecia bitowego w prawo*/
 
-LZespolona operator += (LZespolona Skl1, LZespolona Skl2);
-
-LZespolona operator+(LZespolona Skl1, LZespolona Skl2); /*opearcja dodania dwoczh liczb zespolonych*/
-
-LZespolona operator/(LZespolona Skl1, double Skl2); /* operacja dzielenia liczby zespolonej przez skalar*/
-
-LZespolona operator-(LZespolona Skl1, LZespolona Skl2); /*operacja odjecia dwoch liczb zespolonych*/
-
-LZespolona operator/(LZespolona Skl1, LZespolona Skl2); /*operacja dzielenia dwoch liczb zespolonych*/
-
-LZespolona operator*(LZespolona Skl1, LZespolona Skl2); /* operacje mnozenia dwoch liczb zespolonych*/
-
-LZespolona operator/=(LZespolona Skl1, LZespolona Skl2);
-
-LZespolona operator/=(LZespolona Skl1, double Skl2);
-/*
-*
-*/
-
-void Sprzezenie(LZespolona &Skl2); /*Fukcja tworzaca sprzezenie liczby zespolonej*/
-
-std::istream &operator>>(std::istream &StrmWej, LZespolona &LZesp); /*Funkcja przeciazenia operatora przesuniecia bitowego w prawo*/
-
-std::ostream &operator<<(std::ostream &StrmWyj, LZespolona LZesp); /*Funkcja przeciazenia operatora przesuniecia bitowego w lewo*/
-
-bool operator==(LZespolona Skl1, LZespolona Skl2); /*Funkja porownujaca dwie liczby zespolone*/
-
-void Wyswietl(LZespolona Skl1); /*Funkcja wyswietlajaca liczbe zespolona*/
+std::ostream &operator<<(std::ostream &StrmWyj, LZespolona Lzesp); /*Funkcja przeciazenia operatora przesuniecia bitowego w lewo*/
 
 void WczytajTenZnak(std::istream &StrmWej, char Znak); /**/
 
-double modul2(LZespolona Skl2); /*Funkcja tworzaca modul w postaci skalarnej z liczby zespolonej podniesiony do kwadratu*/
-
+bool operator==(LZespolona Skl1, LZespolona Skl2); /*Funkja porownujaca dwie liczby zespolone*/
 #endif

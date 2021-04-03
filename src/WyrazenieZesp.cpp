0b47c6f76a0945c168175703e92adc29ae0a3c4e
Pawelwysoczanski <80000484+Pawelwysoczanski@users.noreply.github.com>
2021-04-03 17:53:15 +0200

    Update WyrazenieZesp.cpp

--- src/WyrazenieZesp.cpp
+++ src/WyrazenieZesp.cpp
@@ -3,31 +3,34 @@
 #include "WyrazenieZesp.hh"
 #include "LZespolona.hh"
 
-// WyrazenieZesp WcztajWyrz(WyrazenieZesp Wyrz)
+// WyrazenieZesp WyrazenieZesp::WczytajWyrz()
 // {
+
+//     WyrazenieZesp Wyrz;
+
 //     std::cout << std::endl;
 //     std::cin >> Wyrz.Arg1;
 //     std::cin >> Wyrz.Op;
 //     std::cin >> Wyrz.Arg2;
 // }
 
-LZespolona Oblicz(WyrazenieZesp Wyrz)
+LZespolona WyrazenieZesp::Oblicz()
 {
     LZespolona Wynik;
 
-    switch (Wyrz.Op)
+    switch (this->Op)
     {
     case Op_Dodaj:
-        Wynik = Wyrz.Arg1 + Wyrz.Arg2;
+        Wynik = this->Arg1 + this->Arg2;
         break;
     case Op_Odejmij:
-        Wynik = Wyrz.Arg1 - Wyrz.Arg2;
+        Wynik = this->Arg1 - this->Arg2;
         break;
     case Op_Mnoz:
-        Wynik = Wyrz.Arg1 * Wyrz.Arg2;
+        Wynik = this->Arg1 * this->Arg2;
         break;
     case Op_Dziel:
-        Wynik = Wyrz.Arg1 / Wyrz.Arg2;
+        Wynik = this->Arg1 / this->Arg2;
         break;
     default:
         break;
@@ -35,29 +38,11 @@ LZespolona Oblicz(WyrazenieZesp Wyrz)
     return Wynik;
 }
 
-void Wyswietl(WyrazenieZesp Wyrz)
+void WyrazenieZesp::Wyswietl()
 {
-    std::cout << "(" << Wyrz.Arg1 << Wyrz.Op << Wyrz.Arg2 << "i"
+    std::cout << "(" << Arg1 << Op << Arg2 << "i"
               << ")";
 }
-
-// std::istream &operator>>(std::istream &StrmWej, Operator &WczytSym)
-// {
-//     Operator TabTypOp[] = {Op_Dodaj, Op_Odejmij, Op_Dziel, Op_Mnoz};
-//     const char TabSymOp[] = "+-/*", *wSymOp;
-//     char CzytSymOp = 'x';
-
-//     StrmWej >> CzytSymOp;
-//     if ((wSymOp = strchr(wSymOp, CzytSymOp)) == nullptr)
-//     {
-//         StrmWej.setstate(std::ios::failbit);
-//     }
-//     else
-//     {
-//         WczytSym = TabTypOp[wSymOp - TabSymOp];
-//     }
-//     return StrmWej;
-// }
 
 std::ostream &operator<<(std::ostream &StrmWyj, WyrazenieZesp Skl1)
 {
@@ -83,29 +68,28 @@ std::ostream &operator<<(std::ostream &StrmWyj, WyrazenieZesp Skl1)
 
     return StrmWyj;
 }
-
-std::istream &operator>>(std::istream &stream, WyrazenieZesp &Skl1)
+std::istream &operator>>(std::istream &StrmWej, WyrazenieZesp Wyrz)
 {
     char oper;
-    stream >> Skl1.Arg1;
-    stream >> oper;
+    StrmWej >> Wyrz.Arg1;
+    StrmWej >> oper;
     switch (oper)
     {
     case '+':
-        Skl1.Op = Op_Dodaj;
+        Wyrz.Op = Op_Dodaj;
         break;
     case '-':
-        Skl1.Op = Op_Odejmij;
+        Wyrz.Op = Op_Odejmij;
         break;
     case '*':
-        Skl1.Op = Op_Mnoz;
+        Wyrz.Op = Op_Mnoz;
         break;
     case '/':
-        Skl1.Op = Op_Dziel;
+        Wyrz.Op = Op_Dziel;
         break;
     default:
         break;
     }
-    stream >> Skl1.Arg2;
-    return stream;
+    StrmWej >> Wyrz.Arg2;
+    return StrmWej;
 }
